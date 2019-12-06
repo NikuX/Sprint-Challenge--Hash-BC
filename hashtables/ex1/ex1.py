2f5384d8258fc3fbc9cb320b159b002bd5fb691c
NikuX <44727534+NikuX@users.noreply.github.com>
2019-12-06 18:59:37 +0000

    SC hash and blockchain

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,20 +1,24 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i, weight in enumerate(weights):
+        hash_table_insert(ht, weight, i)
+
+    for i, weight in enumerate(weights):
+        result = hash_table_retrieve(ht, limit - weight)
+        if result is not None:
+            if result > i:
+                return result, i
+            else:
+                return i, result
 
     return None
-
 
 def print_answer(answer):
     if answer is not None:
